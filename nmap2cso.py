dc8086c61e98ae5a28715088b61758e90e9504a1
root <root@localhost.localdomain>
2017-01-26 12:39:50 -0600

    fixed nmap2cso to account for missing records

--- nmap2cso.py
+++ nmap2cso.py
@@ -10,6 +10,7 @@ results.open(argv[1:])
 dataStr = dumps(results.data)
 funcStr = """
 function addRecords(records) {
+    var addButton = document.querySelector('#tabContent > form > fieldset:nth-child(8) > table:nth-child(5) > tbody:nth-child(1) > tr > td:nth-child(2) > table > tbody:nth-child(3) a');
     var rows = document.querySelectorAll('.RowdiscoveredOpenPorts');    
     var discoverd = [].map.call(rows, function(r) {
         return [r.querySelector('input[name="eptOpenPortIPs"]').value, r.querySelector('input[name="eptOpenPortPorts"]').value, r.querySelector('input[name="eptOpenPortIdentifiedTypeVersions"]').value];
@@ -18,7 +19,16 @@ function addRecords(records) {
         var matchPosition = discovered.findIndex(function(r) {
             return r[0] == rec[0] && r[1] == rec[1];
         });
-        rows[matchPosition].querySelector('input[name="eptOpenPortIdentifiedTypeVersions"]').value = rec[3];
+        if(matchPosition < 0) {
+            addButton.click();
+            var newrows = rows[0].parentNode.children;
+            var newrow = newrows[newrows.length -1 ];
+            newrow.querySelector('input[name="eptOpenPortIPs"]').value = rec[0];
+            newrow.querySelector('input[name="eptOpenPortPorts"]').value = rec[1];
+            newrow.querySelector('input[name="eptOpenPortIdentifiedTypeVersions"]').value = rec[3];
+        } else {
+            rows[matchPosition].querySelector('input[name="eptOpenPortIdentifiedTypeVersions"]').value = rec[3];
+        }
     });
 }"""
 
